36a0d01a5ef3461c713f9ced59034f1d8f24d1d3
Michael Apim <115515283+mdapim@users.noreply.github.com>
2023-12-20 21:52:52 +0000

    chore: remove example folder

--- othello/example/code.js
+++
@@ -1,9 +0,0 @@
-const author = {
-  name: 'Jan',
-  github: 'jamuhl'
-}
-i18next.t('key', { author })
-// -> "I am Jan"
-
-i18next.t('key', { what: 'i18next', how: 'great' })
-// -> "i18next is great"
